13a2d5cced975faf995d8c9240a7bd25d5db2d9a
sihams <aitoumgharsiham@gmail.com>
2024-12-22 23:37:01 +0100

    start project

---
+++ src/test/java/com/siham/AppTest.java
@@ -0,0 +1,11 @@
+package com.siham;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
